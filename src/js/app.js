7d6a1d9f5a7bfbfd03d482d6dbf93ce07a2a728b
Nstampfli <nathan.stampfli@hetic.net>
2020-08-02 05:08:05 +0200

    random word and create the good number of _

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,30 @@
-console.log('test link js');
+const $letters = document.querySelectorAll(".pendu__letter");
+const $wordsDiv = document.querySelector(".pendu__words")
+const listOfWords = ['voiture', 'maison', 'ventilateur', 'air', 'ciel', 'pikachu'];
+
+
+(function play() {
+  randomNumber();
+}());
+
+function randomNumber() {
+  let randomNumber = Math.floor(Math.random() * listOfWords.length);
+  let wordWanted = listOfWords[randomNumber];
+  createWordHide(wordWanted.length);
+}
+
+function createWordHide(number) {
+  for (let i = 0; i < number; i++) {
+    $wordsDiv.innerHTML += "<li class='pendu__word'> _ </li>"
+  }
+}
+
+
+
+
+for (let i = 0; i < $letters.length; i++) {
+  $letters[i].addEventListener('click', () =>{
+    let ab= $letters[i].textContent; 
+  });
+}
+
